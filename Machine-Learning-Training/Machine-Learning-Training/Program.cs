8f7aa05188771e571e1e658e8ac7ac659f7a1e9f
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2024-01-19 19:27:27 -0500

    Project Initialization
    - Initialize Project

---
+++ Machine-Learning-Training/Machine-Learning-Training/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 10000; i ++)
+    Console.WriteLine("Neural Network Project!");
